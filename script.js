661739f07e1e26f67bec45be1e1fc6b01711719f
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-05-18 11:34:58 -0400

    update to only flip two

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+//variable to represent what state you are in - 1st/2nd
+var state = "";
 
 $(function () {
   $(".tile").click(function () {
-    //add the class "open" to flip tile open
-    $(this).addClass("open");
+    //if you are in first state, flip card
+    if (state === "") {
+      state = "first";
+      //add the class "open" to flip tile open
+      $(this).addClass("open");
+    } else if (state === "first") {
+      state = "second";
+      //add the class "open" to flip tile open
+      $(this).addClass("open");
+    }
+
   });
 });
