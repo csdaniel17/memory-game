15622dcbb4a3a3cd819697559caf17cbb5fb7f1a
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-05-18 15:05:02 -0400

    fix bugs

--- script.js
+++ script.js
@@ -15,16 +15,37 @@ $(function () {
     } else if (state === "second") {
       card2 = $(this).find("img").attr("src");
       state = "first";
+      //count moves
+      count += 1;
+      //display moves after two clicks
+      $("#num-moves").text(count);
       //set timeout to flip cards back over after if not match
       if (card1 != card2) {
         setTimeout(function () {
           $(".tile").removeClass("open");
         }, 800);
       } else {
+        //if they are a match, add class "match" and remove "open"
         $(".open").addClass("match");
         $(".open").removeClass("open");
       }
     }
+    var matched = $(".match").length;
+    var totalCards = $(".tile").length;
+    if (matched === totalCards) {
+      $("#game-over").show();
+    }
+  });
 
+  //reset game
+  $("#new").click(function () {
+    //reset counter
+    count = 0;
+    $("#num-moves").text(count);
+    //reset congrats message
+    $("#game-over").hide();
+    //remove "match" and "open" class
+    $(".open").removeClass("open");
+    $(".match").removeClass("match");
   });
 });
