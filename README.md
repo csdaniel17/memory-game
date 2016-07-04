541d6f2a146231aa8be78b723d9e9b41524ba70b
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-07-04 13:46:45 -0400

    update README

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-This is a monster memory card game using jQuery.
+# Monster Memory Game
+
+## Overview
+* A memory concentration game built using jQuery where you must flip over tiles to make a match. Flip two tiles over to see if they are a match. If not, the tiles will flip back over, but if you got a match then they stay! Try to match all the pairs!
+
+![Monster Memory Game](images/memorygame.png)
+
+
+### Technologies, frameworks, & programming Languages used
+* HTML5 & CSS3
+* JavaScript
+* jQuery
+* Lodash
+
+
+### Url to live demo
 
 [Check it out](http://danielmemorygame.surge.sh/)
--- script.js
+++ script.js
@@ -5,77 +5,77 @@ var card1;
 var card2;
 
 $(function () {
-//shuffle cards
-var pictures = [
-  'monsters-01.png',
-  'monsters-02.png',
-  'monsters-03.png',
-  'monsters-04.png',
-  'monsters-05.png',
-  'monsters-06.png',
-  'monsters-07.png',
-  'monsters-08.png',
-  'monsters-09.png',
-  'monsters-10.png',
-  'monsters-11.png',
-  'monsters-12.png',
-  'monsters-13.png',
-  'monsters-14.png',
-  'monsters-15.png',
-  'monsters-16.png'
-];
+  //shuffle cards
+  var pictures = [
+    'monsters-01.png',
+    'monsters-02.png',
+    'monsters-03.png',
+    'monsters-04.png',
+    'monsters-05.png',
+    'monsters-06.png',
+    'monsters-07.png',
+    'monsters-08.png',
+    'monsters-09.png',
+    'monsters-10.png',
+    'monsters-11.png',
+    'monsters-12.png',
+    'monsters-13.png',
+    'monsters-14.png',
+    'monsters-15.png',
+    'monsters-16.png'
+  ];
 
-pictures = _.shuffle(pictures);
+  pictures = _.shuffle(pictures);
 
-var fourPics = [];
-fourPics = pictures.slice(0, 4);
+  var fourPics = [];
+  fourPics = pictures.slice(0, 4);
 
-var eightPics = [];
-eightPics = fourPics.concat(fourPics);
+  var eightPics = [];
+  eightPics = fourPics.concat(fourPics);
 
-shuffledPics = _.shuffle(eightPics);
+  shuffledPics = _.shuffle(eightPics);
 
-function shuffled() {
-  for (var i = 0; i < shuffledPics.length; i++) {
-    $('#grid').append('<div class="tile"><img class="monster" src="images/' + shuffledPics[i] + '"><div class="back"></div></div>');
-  }
-
-  $(".tile").click(function () {
-    if ($(this).hasClass("open")) {
-      return;
+  function shuffled() {
+    for (var i = 0; i < shuffledPics.length; i++) {
+      $('#grid').append('<div class="tile"><img class="monster" src="images/' + shuffledPics[i] + '"><div class="back"></div></div>');
     }
-    //find card src and flip card when clicked
-    if (state === "first") {
-      card1 = $(this).find("img").attr("src");
-      $(this).addClass("open");
-      state = "second";
-    } else if (state === "second") {
-      card2 = $(this).find("img").attr("src");
-      $(this).addClass("open");
-      state = "first";
-      //count moves
-      count++;
-      //display moves after two clicks
-      $("#num-moves").text(count);
-      //set timeout to flip cards back over after if not match
-      if (card1 != card2) {
-        setTimeout(function () {
-          $(".tile").removeClass("open");
-        }, 500);
-      } else {
-        //if they are a match, add class "match" and remove "open"
-        $(".open").addClass("match");
-        $(".open").removeClass("open");
+
+    $(".tile").click(function () {
+      if ($(this).hasClass("open")) {
+        return;
       }
-    }
-    var matched = $(".match").length;
-    var totalCards = $(".tile").length;
-    if (matched === totalCards) {
-      $("#game-over").show();
-    }
-  });
-}
-shuffled();
+      //find card src and flip card when clicked
+      if (state === "first") {
+        card1 = $(this).find("img").attr("src");
+        $(this).addClass("open");
+        state = "second";
+      } else if (state === "second") {
+        card2 = $(this).find("img").attr("src");
+        $(this).addClass("open");
+        state = "first";
+        //count moves
+        count++;
+        //display moves after two clicks
+        $("#num-moves").text(count);
+        //set timeout to flip cards back over after if not match
+        if (card1 != card2) {
+          setTimeout(function () {
+            $(".tile").removeClass("open");
+          }, 500);
+        } else {
+          //if they are a match, add class "match" and remove "open"
+          $(".open").addClass("match");
+          $(".open").removeClass("open");
+        }
+      }
+      var matched = $(".match").length;
+      var totalCards = $(".tile").length;
+      if (matched === totalCards) {
+        $("#game-over").show();
+      }
+    });
+  }
+  shuffled();
   //reset game
   $("#new").click(function () {
     $('#grid').html('');
